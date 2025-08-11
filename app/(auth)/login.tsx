ceb963477d445c97851c816a544bd9510ca3f21e
Punitx099 <punitchaudhary8471@gmail.com>
2025-08-11 19:10:25 +0530

    feat: add login and register screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,15 +1,126 @@
+import BackButton from "@/components/BackButton";
+import Button from "@/components/Button";
+import Input from "@/components/Input";
 import ScreenWrapper from "@/components/ScreenWrapper";
-import React from "react";
-import { StyleSheet, Text } from "react-native";
+import Typo from "@/components/Typo";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+
+import React, { useRef, useState } from "react";
+import { Alert, Pressable, StyleSheet, View } from "react-native";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async () => {
+    if (!emailRef.current || !passwordRef) {
+      Alert.alert("Login", "Please fill all the fields");
+      return;
+    }
+    console.log("email:", emailRef.current);
+    console.log("password:", passwordRef.current);
+    console.log("good to go");
+  };
   return (
     <ScreenWrapper>
-      <Text>Login</Text>
+      <View style={styles.container}>
+        <BackButton iconSize={28} />
+
+        <View style={{ gap: 5, marginTop: spacingY._20 }}>
+          <Typo size={30} fontWeight={"800"}>
+            Hey,
+          </Typo>
+          <Typo size={30} fontWeight={"800"}>
+            Welcome Back
+          </Typo>
+        </View>
+        {/* form */}
+        <View style={styles.form}>
+          <Typo size={16} color={colors.textLight}>
+            Login now to track all your expenses
+          </Typo>
+          <Input
+            placeholder="Enter your email"
+            onChangeText={(value) => (emailRef.current = value)}
+            icon={
+              <Ionicons
+                name="search"
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Input
+            placeholder="Enter your password"
+            secureTextEntry
+            onChangeText={(value) => (passwordRef.current = value)}
+            icon={
+              <Ionicons
+                name="lock-closed"
+                size={verticalScale(26)}
+                color={colors.neutral300}
+                weight="fill"
+              />
+            }
+          />
+          <Typo size={14} color={colors.text} style={{ alignSelf: "flex-end" }}>
+            Forgot Password?
+          </Typo>
+          <Button loading={isLoading} onPress={handleSubmit}>
+            <Typo fontWeight={700} color={colors.black} size={21}>
+              Login
+            </Typo>
+          </Button>
+        </View>
+        <View style={styles.footer}>
+          <Typo size={15}>Don't have an account?</Typo>
+          <Pressable onPress={() => router.navigate("/(auth)/register")}>
+            <Typo size={15} fontWeight={700} color={colors.primary}>
+              Sign up
+            </Typo>
+          </Pressable>
+        </View>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,14 +1,37 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { colors, radius } from "@/constants/theme";
+import { BackButtonProps } from "@/types";
+import { verticalScale } from "@/utils/styling";
 
-const BackButton = () => {
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import React from "react";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+const BackButton = ({ style, iconSize = 26 }: BackButtonProps) => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>BackButton</Text>
-    </View>
+    <TouchableOpacity
+      onPress={() => router.back()}
+      style={[styles.button, style]}
+    >
+      <Ionicons
+        name="chevron-back"
+        size={verticalScale(iconSize)}
+        color={colors.white}
+        weight="bold"
+      />
+    </TouchableOpacity>
   );
 };
 
 export default BackButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: colors.neutral600,
+    alignSelf: "flex-start",
+    borderRadius: radius._12,
+    borderCurve: "continuous",
+    padding: 5,
+  },
+});
