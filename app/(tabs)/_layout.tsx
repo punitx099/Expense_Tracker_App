6e7efbf71140c46ce296ffd474d9972ad48b51d9
Punitx099 <punitchaudhary8471@gmail.com>
2025-08-14 16:57:29 +0530

    refactor(tabs): update _layout and index for new tab structure

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,17 @@
+import CustomsTabs from "@/components/CustomsTabs";
 import { Tabs } from "expo-router";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const _layout = () => {
-  return <Tabs></Tabs>;
+  return (
+    <Tabs tabBar={CustomsTabs} screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="statistics" />
+      <Tabs.Screen name="wallet" />
+      <Tabs.Screen name="profile" />
+    </Tabs>
+  );
 };
 
 export default _layout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,27 @@
 import Button from "@/components/Button";
+import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { auth } from "@/config/firebase";
 import { colors } from "@/constants/theme";
+import { useAuth } from "@/contexts/authContext";
 import { signOut } from "firebase/auth";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 const Home = () => {
+  const { user } = useAuth();
+
+  console.log("user:", user);
   const handleLogout = async () => {
     await signOut(auth);
   };
   return (
-    <View>
-      <Text>Home </Text>
+    <ScreenWrapper>
+      <Typo>Home </Typo>
       <Button onPress={handleLogout}>
         <Typo color={colors.black}>Logout</Typo>
       </Button>
-    </View>
+    </ScreenWrapper>
   );
 };
 
