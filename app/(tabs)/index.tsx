97afee742f7663ec1b0564f8bbf9763a15564bd7
Punitx099 <punitchaudhary8471@gmail.com>
2025-08-14 16:33:08 +0530

    feat: add profileModal in modals

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,22 @@
-import Button from "@/components/Button";
 import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
-import { auth } from "@/config/firebase";
-import { colors } from "@/constants/theme";
 import { useAuth } from "@/contexts/authContext";
-import { signOut } from "firebase/auth";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const Home = () => {
   const { user } = useAuth();
 
-  console.log("user:", user);
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
+  // console.log("user:", user);
+  // const handleLogout = async () => {
+  //   await signOut(auth);
+  // };
   return (
     <ScreenWrapper>
       <Typo>Home </Typo>
-      <Button onPress={handleLogout}>
+      {/* <Button onPress={handleLogout}>
         <Typo color={colors.black}>Logout</Typo>
-      </Button>
+      </Button> */}
     </ScreenWrapper>
   );
 };
