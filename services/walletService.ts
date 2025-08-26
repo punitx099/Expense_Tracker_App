14579068a04982b08fd8e9da9c2a641bc06fafef
Punitx099 <punitchaudhary8471@gmail.com>
2025-08-26 22:04:08 +0530

    feat:create update wallet and give the function to response and save data and image in cloudinary and firestore

---
+++ services/walletService.ts
@@ -0,0 +1,45 @@
+import { firestore } from "@/config/firebase";
+import { ResponseType, WalletType } from "@/types";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageService";
+
+export const createOrUpdateWallet = async (
+  walletData: Partial<WalletType>
+): Promise<ResponseType> => {
+  try {
+    let walletTOSave = { ...walletData };
+
+    if (walletData.image) {
+      const imageUploadRes = await uploadFileToCloudinary(
+        walletData.image,
+        "wallets"
+      );
+
+      if (!imageUploadRes.success) {
+        return {
+          success: false,
+          msg: imageUploadRes.msg || "Failed to upload wallet icon",
+        };
+      }
+      walletTOSave.image = imageUploadRes.data;
+    }
+
+    if (!walletData?.id) {
+      // new wallet
+      walletTOSave.amount = 0;
+      walletTOSave.totalIncome = 0;
+      walletTOSave.totalExpenses = 0;
+      walletTOSave.created = new Date();
+    }
+
+    const walletRef = walletData?.id
+      ? doc(firestore, "wallets", walletData?.id)
+      : doc(collection(firestore, "wallets"));
+
+    await setDoc(walletRef, walletTOSave, { merge: true }); //update only the data provided
+    return { success: true, data: { ...walletTOSave, id: walletRef.id } };
+  } catch (error: any) {
+    console.log("error creating or updating wallet:", error);
+    return { success: false, msg: error.message };
+  }
+};
