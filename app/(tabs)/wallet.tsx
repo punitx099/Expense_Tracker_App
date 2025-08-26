eb42dcfbfb87bdecab119a5283c1fa7807cd2e17
Punitx099 <punitchaudhary8471@gmail.com>
2025-08-26 21:52:27 +0530

    feat:update wallet screen and wallet modal layout

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,14 +1,85 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import { Ionicons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 const Wallet = () => {
+  const router = useRouter();
+  const getTotalBalance = () => {
+    return 2345;
+  };
   return (
-    <View>
-      <Text>Wallet</Text>
-    </View>
+    <ScreenWrapper style={{ backgroundColor: colors.black }}>
+      <View style={styles.container}>
+        {/* {balance view} */}
+        <View style={styles.balanceView}>
+          <View style={{ alignItems: "center" }}>
+            <Typo size={45} fontWeight={"500"}>
+              ${getTotalBalance()?.toFixed(2)}
+            </Typo>
+            <Typo size={16} color={colors.neutral300}>
+              Total Balance
+            </Typo>
+          </View>
+        </View>
+        {/* {wallet} */}
+        <View style={styles.wallets}>
+          {/* {header} */}
+          <View style={styles.flexRow}>
+            <Typo size={20} fontWeight={"500"}>
+              My Wallets
+            </Typo>
+            <TouchableOpacity
+              onPress={() => router.push("/(modals)/walletModal")}
+            >
+              <Ionicons
+                name="add-circle"
+                weight="fill"
+                color={colors.primary}
+                size={verticalScale(33)}
+              />
+            </TouchableOpacity>
+          </View>
+          {/* {todo: wallets list} */}
+        </View>
+      </View>
+    </ScreenWrapper>
   );
 };
 
 export default Wallet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  balanceView: {
+    height: verticalScale(160),
+    backgroundColor: colors.black,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+  wallets: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    borderTopRightRadius: radius._30,
+    borderTopLeftRadius: radius._30,
+    padding: spacingX._20,
+    paddingTop: spacingX._25,
+  },
+  listStyle: {
+    paddingVertical: spacingY._25,
+    padding: spacingY._15,
+  },
+});
