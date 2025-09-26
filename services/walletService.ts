40fc888a3fd89b9753da2424715e9584aa438c7e
Punitx099 <punitchaudhary8471@gmail.com>
2025-09-26 23:43:59 +0530

    create update and delete wallet services

--- services/walletService.ts
+++ services/walletService.ts
@@ -1,6 +1,15 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, WalletType } from "@/types";
-import { collection, deleteDoc, doc, setDoc } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  setDoc,
+  where,
+  writeBatch,
+} from "firebase/firestore";
 import { uploadFileToCloudinary } from "./imageService";
 
 export const createOrUpdateWallet = async (
@@ -49,8 +58,47 @@ export const deleteWallet = async (walletId: string): Promise<ResponseType> => {
     const walletRef = doc(firestore, "wallets", walletId);
     await deleteDoc(walletRef);
 
-    //todo: delete all transactions related to this wallet
+    deleteTransactionByWalletId(walletId);
     return { success: true, msg: "Wallet deleted successfully" };
+  } catch (err: any) {
+    console.log("error deleting wallet:", err);
+    return { success: false, msg: err.message };
+  }
+};
+export const deleteTransactionByWalletId = async (
+  walletId: string
+): Promise<ResponseType> => {
+  try {
+    let hasMoreTransactions = true;
+
+    while (hasMoreTransactions) {
+      const transactionsQuery = query(
+        collection(firestore, "transaction"),
+        where("walletId", "==", walletId)
+      );
+
+      const transactionSnapshot = await getDocs(transactionsQuery);
+      if (transactionSnapshot.size == 0) {
+        hasMoreTransactions = false;
+        break;
+      }
+
+      const batch = writeBatch(firestore);
+
+      transactionSnapshot.forEach((transactionDoc) => {
+        batch.delete(transactionDoc.ref);
+      });
+
+      await batch.commit();
+
+      console.log(
+        `${transactionSnapshot.size} transaction deleted in this batch`
+      );
+    }
+    return {
+      success: true,
+      msg: "All transaction deleted successfully",
+    };
   } catch (err: any) {
     console.log("error deleting wallet:", err);
     return { success: false, msg: err.message };
